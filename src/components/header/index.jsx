67fdbddc29594c2b38fb7e78fc2ae02cc2f0afc6
gang tian <444202104@qq.com>
2025-01-13 13:41:50 +0800

    feat: nav部分

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+import styles from './index.module.scss';
+
 export default function Header () {
+
+  const [navItems, setNavItems] = useState([
+    { id: 1, navName: '首页', pathTo: '/' },
+    { id: 2, navName: '产品', pathTo: '/product' },
+    { id: 3, navName: '建模', pathTo: '/model' },
+    { id: 4, navName: '编程', pathTo: '/code' },
+    { id: 5, navName: '最新消息', pathTo: '/news' },
+    { id: 6, navName: '联系', pathTo: '/contact' },
+  ])
+
   return (
-    <div>
-      头部
+    <div className={styles.header}>
+      <div className={styles.leftbox}>
+        <div className={styles.logo}>
+          <img src="" alt="logo" />
+        </div>
+        {
+          navItems?.map((nav) => (
+            <div className={styles.navName} key={nav.id}>{nav.navName}</div>
+          ))
+        }
+      </div>
+      <div className={styles.rightbox}>
+        <div className={styles.icons}>
+          <img src="" alt="搜索" />
+        </div>
+        <div className={styles.icons}>
+          <img src="" alt="购物车" />
+        </div>
+        <div className={styles.icons}>
+          <img src="" alt="个人中心" />
+        </div>
+      </div>
     </div>
   )
 }
