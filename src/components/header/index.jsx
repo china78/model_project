764486b1afb4f929c4a4211cb0bb583208190578
gang tian <444202104@qq.com>
2025-01-13 14:46:57 +0800

    feat: 个人中心

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import styles from './index.module.scss';
+import logo from '@/assets/icons/logo.png';
+import search from '@/assets/icons/search.png';
+import shop from '@/assets/icons/shop.png';
+import me from '@/assets/icons/me.png';
 
 export default function Header () {
 
@@ -11,12 +15,13 @@ export default function Header () {
     { id: 5, navName: '最新消息', pathTo: '/news' },
     { id: 6, navName: '联系', pathTo: '/contact' },
   ])
+  
 
   return (
     <div className={styles.header}>
       <div className={styles.leftbox}>
         <div className={styles.logo}>
-          <img src="" alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
         {
           navItems?.map((nav) => (
@@ -26,13 +31,13 @@ export default function Header () {
       </div>
       <div className={styles.rightbox}>
         <div className={styles.icons}>
-          <img src="" alt="搜索" />
+          <img src={search} alt="搜索" />
         </div>
         <div className={styles.icons}>
-          <img src="" alt="购物车" />
+          <img src={shop} alt="购物车" />
         </div>
         <div className={styles.icons}>
-          <img src="" alt="个人中心" />
+          <img src={me} alt="个人中心" />
         </div>
       </div>
     </div>
